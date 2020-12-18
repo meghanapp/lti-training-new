e2551a29ddad5712e7f4e239f9ae5330b81c467b
megha <megha@DESKTOP-M50QMMU>
2020-12-18 16:07:26 +0530

    first commit

---
+++ DemoApp/src/com/lti/model/Sample.java
@@ -0,0 +1,10 @@
+package com.lti.model;
+
+public class Sample {
+     public void show() {
+    	 System.out.println("Hello World");
+     }
+     public void method() {
+    	 System.out.println("Hi,how are you doing?");
+     }
+}
