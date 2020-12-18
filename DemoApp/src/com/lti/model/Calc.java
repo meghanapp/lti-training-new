7f4d4fe614adbe107020e39a61ac9e13c2fc30d8
meghanapp <31460697+meghanapp@users.noreply.github.com>
2020-12-18 16:55:14 +0530

    Update Calc.java

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -4,4 +4,8 @@ public class Calc {
      public void add(int num1,int num2) {
     	 System.out.println("summation="+(num1+num2));
      }
+     public void prod(int num1,int num2) {
+    	 System.out.println("product="+(num1+num2));
+     }
+     
 }
