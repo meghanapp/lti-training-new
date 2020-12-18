2ee4245a993c3663de7b433b3149e2f49f5c3b88
megha <megha@DESKTOP-M50QMMU>
2020-12-18 16:53:47 +0530

    added subs method

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -4,4 +4,8 @@ public class Calc {
      public void add(int num1,int num2) {
     	 System.out.println("summation="+(num1+num2));
      }
+     public void subs(int num1,int num2) {
+    	 System.out.println("difference="+(num1-num2));
+
+	}
 }
